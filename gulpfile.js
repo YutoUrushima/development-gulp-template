3209829e0612e3a5abcb9420cd5e2e9841ea4866
yuto urushima <yuto259233@gmail.com>
2021-02-15 22:00:59 +0900

    pug sass コンパイル

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,48 @@
 // 定数の設定
 const {src, dest, watch, series, parallel } = require('gulp');
+const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 
 // パスの設定
 const paths = {
-    root: 'dist/',
+    root: 'src/',
     sass: 'src/assets/styles/**/*.scss',
     css: 'dist/assets/styles/',
+    pug: 'src/**/*.pug',
+    html: 'dist/',
+}
+
+// pug
+const html = (done) => {
+    return src(paths.pug)
+        .pipe(
+        plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+        )
+        .pipe(
+            pug({
+                basedir: paths.root,
+                pretty: true,
+            })
+        )
+        .pipe(dest(paths.html));
+    done();
 }
 
 // sass
-const css = () => {
+const css = (done) => {
     return src(paths.sass)
       .pipe(
         plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
       )
       .pipe(sass())
-      .pipe(dest(paths.css))
+      .pipe(dest(paths.css));
+    done();
 }
 
 exports.default = () => {
-    watch(paths.sass, css)
+    watch(paths.pug, html);
+    watch(paths.sass, css);
 }
+exports.build = parallel(html, css);
