54c730ba3d75995fbc92574f21cdff8f7ba80afc
YutoUrushima <yuto259233@gmail.com>
2022-08-17 17:21:31 +0900

    Fix dependency

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,7 @@
-import sampleOutput from './modules/sample-module';
-import multiplication from './modules/calculator-module';
-
+import sampleOutput from "./modules/sample-module";
+import multiplication from "./modules/calculator-module";
 
 sampleOutput();
-
 
 var item = 10;
 var price = 120;
